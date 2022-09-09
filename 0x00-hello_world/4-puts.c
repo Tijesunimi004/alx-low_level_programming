9e4703eb10476ee3119f9854a7c24ad409e3064a
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-08 18:36:33 -0700

    Mae

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 #include<stdio.h>
 
 int main(void)
 {
-	  puts("\"Programming is like building a multilingual puzzle");
-	 	 return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
