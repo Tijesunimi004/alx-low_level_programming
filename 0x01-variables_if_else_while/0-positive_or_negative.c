6cc02bded3634d01c991754924e243ac7d5674a2
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-10 10:15:47 -0700

    Shuu

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,18 +11,16 @@
 /* betty style doc for function main goes there */
 
 int main(void)
-
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 /* your code goes there */
-if (n>5);
-	printf("%d is positive\n", n);
-else if (n==0);
-       printf("%d is zero\n", n); 	
-else 
-	printf("%d is negative\n", n);
-					return (0);
-
+	if (n>5);
+		printf("%d is positive\n", n);
+	else if (n==0);
+		printf("%d is zero\n", n); 	
+	else 
+		printf("%d is negative\n", n);
+	return (0);
 }
