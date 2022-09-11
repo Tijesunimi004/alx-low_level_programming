717269aafbdd4408fda88f5371107d5f89255199
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-11 10:03:29 -0700

    HMM

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,19 +5,31 @@
 * Return: Always 0 (Success)
 */
 
+
+
 int main(void)
 {
-	int i = 0;
+	int a = 10 - '0';
 
-	while (i < 10)
-	{
-		printf("%i", i);
 
-		i++;
-	}
+		while (a < 10)
+		{
+			putchar(a);
 
-	printf("\n");
+			++a;
+		}
+
+	putchar('\n');
 
 	return (0);
+
+
+
+
+
+
+
+
+
 
 }
