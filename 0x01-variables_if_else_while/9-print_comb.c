e047937a90ebfebd3899b1b67d110fdf86fe3ca2
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-11 21:48:58 -0700

    Combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-* Return: Always 0 (Success)
+* main -prints all single digit numbers of base 10 starting from 0
+* separated by ,
+* Numbers should be printed in ascending order
+* Return: 0 (success)
 */
 
 int main(void)
+
 {
 	int k = 0;
 
-	while (k <= 10)
-
+	while (k < 10)
 	{
-		putchar(k + '0');
-			if (k != 9)
+		putchar(k +  '0');
+			if  (k != 9)
 			{
 				putchar(44);
 				putchar(32);
 			}
 		k++;
 
+
 	}
 
 	putchar('\n');
 	return (0);
+
+
+
+
 
 
 }
