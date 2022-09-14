d86e852ff67d6d090db8cd07f36e077f98f29335
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-14 09:33:34 -0700

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* isalpha - (int c)
+* @c: parameter checked
+* Return: 0(success)
+*/
+
+int isalpha(int c)
+{
+	int i;
+
+	if ((c >= 65 &&  c <= 90) || (c >= 97 && c <= 122))
+	{
+		i = 1;
+	}
+	else
+	{
+		i = 0;
+
+	}
+
+	return (i);
+
+
+}
