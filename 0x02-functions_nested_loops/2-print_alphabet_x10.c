0569fd0f40c49d98a11285a21433b96635e7ec5d
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-14 07:28:10 -0700

    10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - check the code.
+* Return: Always 0.
+*/
+
+int print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		j = 'a';
+		while (j <= 'z')
+		{
+			putchar(j);
+			j++;
+		}
+
+		putchar('\n');
+	}
+
+	return (0);
+}
