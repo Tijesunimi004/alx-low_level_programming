1d1b8dad8b14550451f12b6236500aec1438782f
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-14 13:19:17 -0700

    Absolute Copy

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,29 +1,38 @@
 #include <stdio.h>
-/**
-* _abs - (int)
-*@n: an integer
-*
-* Return: Always 0.
-*/
 
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _abs - computes the absolute value of an integer
+ *
+ *   * @n: value to check
+ *
+ *    * Return: 0 (success)
+ *
+ *     */
 
 int _abs(int n)
+
 {
+
 	int a;
 
-	if (n >= 0)
+
+	if (n < 0)
+				{
+
+	a = -(n);
+
+		return (a);
+
+	}
+
+		else
 	{
 		return (n);
-	}
-	else
-	{
-		a = -(n);
-		return (n);
-	}
-
-
-
-
-
+			}
 
 }
