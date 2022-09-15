6b20cd0b9902516f97a052f501d3c01c6e2ec757
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-15 15:05:15 -0700

    Print all

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* void print_numbers - (void)
+* @void : print all number from 0-9
+* Return: Always 0.
+*/
+
+void print_numbers(void)
+
+{
+		int i;
+
+	        i = 0;
+
+
+
+		for(i = 0; i < 10; i++ )
+
+		putchar(i + '0');
+
+}
