baa4a307904c5dd792bd5085e6282ca68d6d31d5
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-15 13:28:55 -0700

    Upper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,13 +9,7 @@
 * the compiler will process its contents twice and it will result in an error
 */
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
+int _isupper(int c);
 int _isdigit(int c);
 
 #endif
