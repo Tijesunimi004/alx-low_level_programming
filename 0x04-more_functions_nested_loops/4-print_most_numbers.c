8021f85c5549a222505767a890bccf03c65425a9
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-15 16:35:49 -0700

    Mostt

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* void print_most_numbers - (void)
+* @void : print most numbers
+* Return: Always 0.
+*/
+
+
+
+
+
+void print_most_numbers(void)
+
+{
+
+	int i;
+
+	i = 0;
+
+
+
+	for(i = 0; i < 10; i++)
+
+	{
+
+		if ((i != 2) && (i != 4))
+
+		{
+
+			putchar(i + '0');
+
+		}
+
+	}
+
+
+
+}
