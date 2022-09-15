18bdb59fa994ce9a617a19cdedeeda59d3f02152
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-15 12:38:50 -0700

    Digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -22,21 +22,3 @@ int _isdigit(int c)
 
 	return (isdigit);
 }
-
-int main(void)
-
-{
-
-char c;
-
-c = '0';
-
-printf("%c: %d\n", c, _isdigit(c));
-
-c = 'a';
-
-printf("%c: %d\n", c, _isdigit(c));
-
-return (0);
-
-}
