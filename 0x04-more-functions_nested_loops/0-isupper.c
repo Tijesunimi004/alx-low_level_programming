fac9e0e6607dfbb03b70124eb0e0116e9e7c5538
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-15 08:18:31 -0700

    UPPER

---
+++ 0x04-more-functions_nested_loops/0-isupper.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+/**
+* _isupper - char x.
+* @x: parameter checked
+* Return: Always 0
+*/
+
+
+
+int _isupper(char x)
+
+{
+		if (x <= 'Z')
+
+		{
+			return (1);
+
+		}
+
+		else
+
+		{
+			return (0);
+		}
+
+}
