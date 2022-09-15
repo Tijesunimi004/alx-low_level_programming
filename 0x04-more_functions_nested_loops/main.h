98e6fa51735bd856911432f18ecf66c77337d0ab
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-15 13:03:05 -0700

    Digit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,59 +7,17 @@
 
 
 /**
- *
- *  *
- *
- *   *  * HEADER FILES
- *
- *    *
- *
- *     *   * Once-only header- If a header file happens to be included twice,
- *
- *      *
- *
- *       *    * the compiler will process its contents twice and it will result in an error
- *
- *        *
- *
- *         *     */
-
-
-
-
-
-
+* HEADER FILES
+* Once-only header- If a header file happens to be included twice,
+* the compiler will process its contents twice and it will result in an error
+*/
 
 int _putchar(char c);
-
-
-
 void print_alphabet(void);
-
-
-
 void print_alphabet_x10(void);
-
-
-
 int _islower(int c);
-
-
-
 int _isalpha(int c);
-
-
-
 int print_sign(int n);
-
-
-
 int _abs(int);
-
-
-
-
-
-
 
 #endif
