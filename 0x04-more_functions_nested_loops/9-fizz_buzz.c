460340d38a5e42e2c4f30d7b66bece46a0e33c41
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-16 12:58:32 -0700

    Triangle

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,16 +6,13 @@
 *  But for multiples of three print Fizz instead of the number
 *  and for the multiples of five print Buzz
 *  For numbers which are multiples of both three and five print FizzBuzz.
-* Return: Always 0.
+* Return: Always 0
 */
 
+
 int main(void)
-
 {
-
-	int i;
-
-	for (i = 1; i <= 100; i++)
+		for(; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && !(i % 5 == 0))
 
