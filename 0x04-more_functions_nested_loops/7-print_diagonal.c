ddf00ee3d843d3f7e2890abb11b50c10dcfa9797
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-16 06:55:35 -0700

    Diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 
 /**
 * print_diagonal - a function that draws a diagonal
 * ine on the terminal.
+* n - lenght of diagonal
 * Return: Always 0.
 *
 */
@@ -11,29 +13,18 @@
 void print_diagonal(int n)
 
 {
+	int i;
 
-	    int i;
+		for (i = 1; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			putchar('\n');
+		}
+		else
+			putchar(92);
+	}
 
-
-
-	    for (i = 1; i <= n; i++)
-
-	    {
-
-		    if (n <= 0)
-
-		    {
-
-			    _putchar('\n');
-
-		    }
-
-		    else
-
-			    _putchar(92);
-
-		    _putchar('\n');
-
-	    }
+		putchar('\n');
 
 }
