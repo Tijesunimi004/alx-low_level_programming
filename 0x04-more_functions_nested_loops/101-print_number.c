4570b6fa30e6670e13e7a504d52f05d717d1ed69
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-16 13:54:38 -0700

    Print

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *
+* void print_number(int n) - Write a function that prints an integer.
+* @int n - number to be printed
+* Return: Always 0
+*/
+
+void print_number(int n)
+{
+
+	putchar(n + '0');
+
+
+
+
+
+
+}
