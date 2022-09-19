7313ed565a67bcf29c6310090cd00ba0911a1784
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-19 14:02:10 -0700

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* void reset_to_98(int *n) -  a function that takes a pointer to an int as parameter
+* and updates the value it points to to 98. 
+* Return: Always 0.
+*/
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,24 @@
+#ifndef _MAIN_H_
+
+#define _MAIN_H_
+
+
+
+
+
+
+
+/**
+ *
+ * * HEADER FILES
+ *
+ * * Once-only header- If a header file happens to be included twice,
+ *
+ * * the compiler will process its contents twice and it will result in an error
+ *
+ * */
+
+
+void reset_to_98(int *n);
+
+#endif
