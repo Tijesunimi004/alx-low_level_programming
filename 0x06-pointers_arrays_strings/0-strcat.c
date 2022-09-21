94b43e8e00bfaadc87bfd68fe458f691aeebb178
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-21 11:36:12 -0700

    STRCAT

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * * *_strcat - his function appends the src string to the dest string,
+ * * overwriting the terminating null byte (\0) at the end of dest,
+ * * and then adds a terminating null byte
+ * * *dest : input string
+ * * *src : input string
+ * * Return: Always 0.
+ * */
+
+
+
+char *_strcat(char *dest, char *src)
+{
+	char *temp = dest;
+
+	while (*dest)
+		dest++;
+
+	while (*src)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,32 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+
+/*
+ *  *
+ *
+ *   * * HEADER FILES
+ *
+ *    *
+ *
+ *     * * Once-only header- If a header file happens to be included twice,
+ *
+ *      *
+ *
+ *       * * the compiler will process its contents twice and it will result in an error
+ *
+ *        *
+ *
+ *         * */
+
+
+
+
+char *_strcat(char *dest, char *src);
+
+
+
+
+
+
+#endif
