53c479250d50289907a1f24fba81eba171bb492f
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-27 05:17:25 -0700

    COPY MEMORY

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* _memcpy - check the code
+* @dest : An array where src will be copied to.
+* @src : The string to be copied.
+* @n : The number of characters to copy.
+* Return: Always 0.
+*/
+
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+
+	unsigned int i;
+
+	for (i = 0; src[i] != '\0' && src[i] <= n; i++)
+
+		dest[i] = src[i];
+
+	return (dest);
+
+}
