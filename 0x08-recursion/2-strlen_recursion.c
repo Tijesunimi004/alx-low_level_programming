c3fb3d4be6bb770d7e4be865a0bc5ab2ebed84f7
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-09-28 20:36:57 -0700

    Length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+
+/**
+* _strlen_recursion -  a function that returns the length of a string..
+* @s : the string to be counted.
+* Return: Always 0.
+*/
+
+
+
+int _strlen_recursion(char *s)
+{
+
+	int len;
+
+	len = 0;
+
+	if (s[len] == '\0')
+
+		return (0);
+
+	len = 1 +  _strlen_recursion(s + 1);
+
+	return (len);
+
+}
