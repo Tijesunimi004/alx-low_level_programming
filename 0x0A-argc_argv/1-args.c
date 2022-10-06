fb235cd843bfe90fc59ca677ab00e5f5d7d76848
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-10-06 13:46:34 -0700

    Args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+ * main - a program that prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: argumrnt vector
+ * Return: 0 (success)
+*/
+
+
+int main(int argc, char  *argv[])
+{
+	printf("%d", atoi(argv[argc - 1]));
+	return (0);
+}
