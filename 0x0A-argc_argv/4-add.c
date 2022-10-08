97faa2e6dc27e0e7c2db29908a6ab79e74c2f4a9
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-10-08 10:05:47 -0700

    Add themm

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
-
 
 /*
 * main -  a program that adds positive numbers.
@@ -11,56 +8,31 @@
 * Return: 0 (success)
 */
 
-
-
 int main(int argc, char *argv[])
-
 {
-
-	    int i = 0;
-
-	        
+		int i = 0;
 
 	        int sum = 0;
 
-
-
 		for(i = 1; i < argc; i++)
-
 		{
-
 			if (i >= 'a' && i <= 'z')
-
 			{
-
 				printf("Error\n");
-
 			}
 
 			else if (argc == 1)
-
 			{
-
 				printf("0\n");
-
 			}
-
-			    
 
 			else
-
 			{
-
 				sum += atoi(argv[i]);
-
 			}
-
 		}
-
-		    
-
 		printf("%d\n", sum);
 
-		return 0;
+		return (0);
 
 }
