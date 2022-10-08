f16e39948b94565392cd390dbe022b4ae4b9e701
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-10-08 10:25:05 -0700

    Add themm

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,9 +12,9 @@ int main(int argc, char *argv[])
 {
 		int i = 0;
 
-	        int sum = 0;
+		int sum = 0;
 
-		for(i = 1; i < argc; i++)
+		for (i = 1; i < argc; i++)
 		{
 			if (i >= 'a' && i <= 'z')
 			{
