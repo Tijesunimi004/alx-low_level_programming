8ac6dd138e5a9d9196f5e3ec44472bb41552cfd3
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-10-21 16:13:02 -0700

    Length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,8 @@
 
 /*
  *
- * list_len - a function that returns the number of elements in a linked list_t list.
+* list_len - a function that returns the number of
+* elements in a linked list_t list.
  *
  * @h: singly linked list
  *
