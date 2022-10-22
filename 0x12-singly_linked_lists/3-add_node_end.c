3874bd438d9af0a392ce35401e340ac7171912f3
Tijesunimi004 <otegbolamarvelrvellous@gmail.com>
2022-10-21 17:30:48 -0700

    Add ENd

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,8 +3,8 @@
 
 
 /**
- *
- *  * add_node_end -  a function that adds a new node at the end of a list_t list.
+ * add_node_end -  a function that adds a new
+ * node at the end of a list_t list.
  *
  *   * @head: previous element in the list
  *
@@ -21,67 +21,36 @@
 list_t *add_node_end(list_t **head, const char *str)
 
 {
-
 	size_t i;
-
-
-
 	list_t *last = *head;
-
 	list_t *new_node = malloc(sizeof(list_t));
 
-
-
 	if (new_node == NULL)
-
 	{
-
 		return (NULL);
-
 	}
-
-
 
 	new_node->str = strdup(str);
 
-
-
 	for (i = 0; str[i] != '\0'; i++)
-
 		;
 
 	new_node->len = i;
-
 	new_node->next = NULL;
 
-
-
 	if (last == NULL)
-
 	{
-
 		*head = new_node;
-
 	}
-
 	else
-
 	{
-
 		while (last->next != NULL)
-
 		{
-
 			last = last->next;
-
 		}
 
-
-
 		last->next = new_node;
-
 	}
-
 	return (*head);
 
 }
